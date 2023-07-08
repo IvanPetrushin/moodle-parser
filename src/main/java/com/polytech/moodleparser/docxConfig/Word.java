159d2da6b4aec4e2e54aa99e098f6c58da8c4436
leunamme4 <90042349+leunamme4@users.noreply.github.com>
2023-07-08 17:26:36 +0300

    WordGeneration added

--- src/main/java/com/polytech/moodleparser/docxConfig/Word.java
+++ src/main/java/com/polytech/moodleparser/docxConfig/Word.java
@@ -101,73 +101,8 @@ public class Word {
     }
 
 
-    public static void main(String[] args) {
+    public static void generateWord(List<Map<String, Map<String, ArrayList<String>>>> parsed) {
         //главный List
-        List<Map<String, Map<String, ArrayList<String>>>> questionsInfo = new ArrayList<>();
-
-        //инициализация тестового массива
-        //вопрос 1
-        String[] answerspr1 = {"rrr", "sss", "sdsds"};
-        ArrayList<String> answers1 = new ArrayList<>(Arrays.asList(answerspr1));
-        Map<String, ArrayList<String>> quAns1 = new HashMap<>();
-        quAns1.put("Вопрос 1?", answers1);
-        Map<String, Map<String, ArrayList<String>>> firstQ = new HashMap<>();
-        firstQ.put("ddwtos", quAns1);
-        questionsInfo.add(firstQ);
-
-
-        //вопрос 2
-        ArrayList<String> answers2 = new ArrayList<>();
-        Map<String, ArrayList<String>> quAns2 = new HashMap<>();
-        quAns2.put("Вопрос 2?", answers2);
-        Map<String, Map<String, ArrayList<String>>> secondQ = new HashMap<>();
-        secondQ.put("essay", quAns2);
-        questionsInfo.add(secondQ);
-
-        //вопрос 3
-        String[] answerspr3 = {"rrr3", "sss3", "sdsds3 - Правильный ответ"};
-        ArrayList<String> answers3 = new ArrayList<>(Arrays.asList(answerspr3));
-        Map<String, ArrayList<String>> quAns3 = new HashMap<>();
-        quAns3.put("Вопрос 3?", answers3);
-        Map<String, Map<String, ArrayList<String>>> thirdQ = new HashMap<>();
-        thirdQ.put("gapselect", quAns3);
-        questionsInfo.add(thirdQ);
-
-        //вопрос 4
-        String[] answerspr4 = {"rrr4", "sss4", "sdsds4 - Правильный ответ"};
-        ArrayList<String> answers4 = new ArrayList<>(Arrays.asList(answerspr4));
-        Map<String, ArrayList<String>> quAns4 = new HashMap<>();
-        quAns4.put("Вопрос 4?", answers4);
-        Map<String, Map<String, ArrayList<String>>> fourthQ = new HashMap<>();
-        fourthQ.put("multichoice", quAns4);
-        questionsInfo.add(fourthQ);
-
-        //вопрос 5
-        String[] answerspr5 = {"rrr5", "sss5 - Правильный ответ", "sdsds5 - Правильный ответ"};
-        ArrayList<String> answers5 = new ArrayList<>(Arrays.asList(answerspr5));
-        Map<String, ArrayList<String>> quAns5 = new HashMap<>();
-        quAns5.put("Вопрос 5?", answers5);
-        Map<String, Map<String, ArrayList<String>>> fifthQ = new HashMap<>();
-        fifthQ.put("multichoiceset", quAns5);
-        questionsInfo.add(fifthQ);
-
-        //вопрос 6
-        String[] answerspr6 = {"1312421", "228"};
-        ArrayList<String> answers6 = new ArrayList<>(Arrays.asList(answerspr6));
-        Map<String, ArrayList<String>> quAns6 = new HashMap<>();
-        quAns6.put("Вопрос 6?", answers6);
-        Map<String, Map<String, ArrayList<String>>> sixthQ = new HashMap<>();
-        sixthQ.put("numerical", quAns6);
-        questionsInfo.add(sixthQ);
-
-        //вопрос 7
-        String[] answerspr7 = {"rrr7", "sss7", "sdsds7"};
-        ArrayList<String> answers7 = new ArrayList<>(Arrays.asList(answerspr7));
-        Map<String, ArrayList<String>> quAns7 = new HashMap<>();
-        quAns7.put("Вопрос 7?", answers7);
-        Map<String, Map<String, ArrayList<String>>> seventhQ = new HashMap<>();
-        seventhQ.put("shortanswer", quAns7);
-        questionsInfo.add(seventhQ);
 
         Map<String, String> types = new HashMap<>();
         types.put("ddwtos", "Перетаскивание в текст");
@@ -183,9 +118,9 @@ public class Word {
             FileOutputStream out = new FileOutputStream(new File("D:\\projetdocx\\abcde.docx")); //надо поменять
 
             //основа
-            for (int i = 0; i < questionsInfo.size(); i++) {
-                Map<String, Map<String, ArrayList<String>>> question = questionsInfo.get(i);
-                int questionNumber = i + 1; //надо поменять с учетом что первый элемент это категория
+            for (int i = 0; i < parsed.size(); i++) {
+                Map<String, Map<String, ArrayList<String>>> question = parsed.get(i);
+                int questionNumber = i; //надо поменять с учетом что первый элемент это категория
                 String questionType = "";
                 String questionText = "";
                 List<String> answers = new ArrayList<>();
